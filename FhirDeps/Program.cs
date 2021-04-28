fda119521792bb88f3052229c5405596cc94a6bf
Gavin Morris <gavin.morris@health.telstra.com>
2021-04-28 08:05:26 +0800

    drop STU3 and add PDS tests

--- FhirDeps/Program.cs
+++ FhirDeps/Program.cs
@@ -1,31 +1,163 @@
-﻿extern alias STU3;
-extern alias R4;
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Hl7.Fhir.Rest;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 
+using Newtonsoft.Json;
+
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Model;
 
 namespace FhirDeps
 {
     class Program
     {
+        static FhirClient CreateClient()
+        {
+            var clientCertificate = new X509Certificate2(
+                @"C:\Ccare\Development\Certificates\NASH\Millennium Health Service - 8003623233353381.p12",
+                "Pass-123");
+
+            var handler = new WebRequestHandler();
+            handler.ClientCertificates.Add(clientCertificate);
+
+            return new FhirClient(
+                "https://sandbox.digitalhealth.gov.au/FhirServerR4-PDA/fhir/",
+                messageHandler: handler);
+        }
+
+        static void FetchPractitionerRole()
+        {
+            Console.WriteLine("\r\n*** PractitionerRole ***\r\n");
+            var client = CreateClient();
+
+            var searchParams = new SearchParams();
+            searchParams.Add("practitioner.name", "kidman");
+
+            searchParams.Include.Add(("PractitionerRole:service", IncludeModifier.None));
+            searchParams.Include.Add(("PractitionerRole:practitioner", IncludeModifier.None));
+            searchParams.Include.Add(("PractitionerRole:location", IncludeModifier.None));
+            searchParams.Include.Add(("PractitionerRole:endpoint", IncludeModifier.None));
+
+            var bundle = client.Search<PractitionerRole>(searchParams);
+
+            foreach (var bundleEntry in bundle.Entry.Where(e => e.Resource is PractitionerRole && e.Search.Mode == Bundle.SearchEntryMode.Match))
+            {
+                var role = bundleEntry.Resource as PractitionerRole;
+                var endpointUrl = ResourceReferenceExtensions.GetAbsoluteUriForReference(role.Endpoint.First(), bundleEntry.FullUrl);
+
+                var service = FindResource<HealthcareService>(bundle, role.HealthcareService.FirstOrDefault());
+                var location = FindResource<Location>(bundle, role.Location.FirstOrDefault());
+                var practitioner = FindResource<Practitioner>(bundle, role.Practitioner);
+                var endpoint = FindResource<Endpoint>(bundle, role.Endpoint.FirstOrDefault());
+
+                var entry = new
+                {
+                    Name = role.Practitioner.Display,
+                    Surname = null as string,
+                    Speciality = role.Specialty.FirstOrDefault()?.Text,
+                    TypeName = service?.Type.FirstOrDefault()?.Text,
+                    Organisation = role.Organization.Display,
+                    Source = endpoint?.ManagingOrganization.Display,
+                    ConnectivityType = endpoint?.ConnectionType.Code,
+                    Location = location?.Name,
+                    ProviderDirectoryUrl = bundleEntry.FullUrl
+                };
+
+                Console.WriteLine(JsonConvert.SerializeObject(entry));
+            }
+        }
+
+        static void FetchHealthcareService()
+        {
+            Console.WriteLine("\r\n*** HealthcareService ***\r\n");
+            var client = CreateClient();
+
+            var searchParams = new SearchParams();
+            searchParams.Add("name", "fernside");
+
+            searchParams.Include.Add(("HealthcareService:organization", IncludeModifier.None));
+            searchParams.Include.Add(("HealthcareService:location", IncludeModifier.None));
+            searchParams.Include.Add(("HealthcareService:endpoint", IncludeModifier.None));
+
+            var bundle = client.Search<HealthcareService>(searchParams);
+
+            foreach (var bundleEntry in bundle.Entry.Where(e => e.Resource is HealthcareService && e.Search.Mode == Bundle.SearchEntryMode.Match))
+            {
+                var service = bundleEntry.Resource as HealthcareService;
+                var endpointUrl = ResourceReferenceExtensions.GetAbsoluteUriForReference(service.Endpoint.First(), bundleEntry.FullUrl);
+
+                var location = FindResource<Location>(bundle, service.Location.FirstOrDefault());
+                var endpoint = FindResource<Endpoint>(bundle, service.Endpoint.FirstOrDefault());
+                var organization = FindResource<Organization>(bundle, service.ProvidedBy);
+
+                var entry = new
+                {
+                    Name = service.Name,
+                    Surname = null as string,
+                    Speciality = service.Specialty.FirstOrDefault()?.Text,
+                    TypeName = service.Type.FirstOrDefault()?.Text,
+                    Organisation = organization?.Name,
+                    Source = endpoint?.ManagingOrganization.Display,
+                    ConnectivityType = endpoint?.ConnectionType.Code,
+                    Location = location?.Name,
+                    ProviderDirectoryUrl = bundleEntry.FullUrl
+                };
+
+                Console.WriteLine(JsonConvert.SerializeObject(entry));
+            }
+        }
+
+        static void SearchPractitionerRole()
+        {
+            var client = CreateClient();
+
+            {
+                var sp = new SearchParams();
+
+                //sp.Add("")
+
+                var bundle = client.Search<PractitionerRole>(sp);
+                foreach (var role in bundle.GetResources().OfType<PractitionerRole>())
+                    Console.WriteLine(role.Practitioner.Display);
+            }
+        }
+
+        static void TestExpand()
+        {
+            //var json = new WebClient().DownloadString("https://r4.ontoserver.csiro.au/fhir/ValueSet/$expand?url=http://snomed.info/sct?fhir_vs=ecl/%3C394658006");
+
+            // http://snomed.info/sct?fhir_vs=ecl%2F%3C394658006&count=1000&activeOnly=true
+            var client = new FhirClient(
+                "https://r4.ontoserver.csiro.au/fhir/");
+
+            var p = new Parameters()
+                .Add("url", new FhirUri("http://snomed.info/sct?fhir_vs=ecl/<394658006"))
+                .Add("count", new Integer(1000))
+                .Add("activeOnly", new FhirBoolean(true));
+            var result = client.TypeOperation<ValueSet>("expand", p, true);
+
+        }
+
         static void Main(string[] args)
         {
-            var client3 = new STU3::Hl7.Fhir.Rest.FhirClient(
-                "https://securemessaging.dev.telstrahealth.com/epd/stu3/fhir");
-            var p3 = new SearchParams();
-            p3.Add("family", "Ellison");
-            var b3 = client3.Search<STU3::Hl7.Fhir.Model.Practitioner>(p3);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            var client4 = new R4::Hl7.Fhir.Rest.FhirClient(
-                "https://sandbox.digitalhealth.gov.au/FhirServerR4-PDA/fhir");
-            var p4 = new SearchParams();
-            p4.Add("family", "Kidman");
-            var b4 = client3.Search<R4::Hl7.Fhir.Model.Practitioner>(p4);
+            var ri = new ResourceIdentity("https://sandbox.digitalhealth.gov.au/FhirServerR4-PDA/fhir/PractitionerRole/97784e9f6c7a489f9d23a40a43161e14");
+
+            FetchPractitionerRole();
+            FetchHealthcareService();
+
+            //SearchPractitionerRole();
+            //TestExpand();
         }
+
+        private static T FindResource<T>(Bundle bundle, ResourceReference reference) =>
+            bundle.Entry
+                .Where(entry => $"{entry.Resource.TypeName}/{entry.Resource.Id}" == reference.Reference)
+                .Select(x => x.Resource)
+                .OfType<T>()
+                .FirstOrDefault();
     }
 }
