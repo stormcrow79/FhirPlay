114b86b3409582699d20ec25a0be0fff40aad4c1
Gavin Morris <gavin.morris@health.telstra.com>
2021-06-01 13:26:37 +0800

    add fetch all resources

--- FhirDeps/Program.cs
+++ FhirDeps/Program.cs
@@ -2,16 +2,19 @@
 using Hl7.Fhir.Rest;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace FhirDeps
 {
     class Program
     {
-        static FhirClient CreateClient()
+        static FhirClient CreateClient(string serverUrl = null)
         {
             var clientCertificate = new X509Certificate2(
                 @"C:\Ccare\Development\Certificates\NASH\Millennium Health Service - 8003623233353381.p12",
@@ -21,7 +24,7 @@ namespace FhirDeps
             //var handler = new WebRequestHandler();
             //handler.ClientCertificates.Add(clientCertificate);
 
-            var client = new FhirClient(
+            var client = new FhirClient(serverUrl ??
                 "https://sandbox.digitalhealth.gov.au/FhirServerR4-PDA/fhir/");
 
             // use OnBeforeRequest for FHIR 1.9 and prior
@@ -29,6 +32,94 @@ namespace FhirDeps
                 e.RawRequest.ClientCertificates.Add(clientCertificate);
             
             return client;
+        }
+
+        private static Bundle PagedSearch<TResource>(
+            FhirClient client,
+            SearchParams searchParams)
+            where TResource : Resource
+        {
+            var bundle = client.Search<TResource>(searchParams);
+
+            if (bundle.NextLink == null)
+                return bundle;
+
+            // keep reading pages until we get to the end, or we hit our result limit
+            var nextUri = bundle.NextLink;
+            var pageIndex = 2;
+            while (nextUri != null)
+            {
+                Thread.Sleep(1000);
+                // TODO: use ILogger
+                Debug.WriteLine($"retrieving search result page {pageIndex++} via {nextUri}");
+
+                var page = client.Get(nextUri) as Bundle
+                           ?? throw new InvalidOperationException("search returned other than Bundle");
+
+                var matches = page.Entry.Where(e => e.Search.Mode == Bundle.SearchEntryMode.Match).ToArray();
+
+                Debug.WriteLine($"{matches.Length} matches ({page.Entry.Count} total)");
+
+                foreach (var entry in page.Entry)
+                {
+                    if (entry.Search.Mode == Bundle.SearchEntryMode.Match ||
+                        !bundle.Entry.Exists(e => e.Resource.TypeName == entry.Resource.TypeName && e.Resource.Id == entry.Resource.Id))
+                        bundle.Entry.Add(entry);
+                }
+
+                nextUri = page.NextLink;
+            }
+
+            Debug.WriteLine($"TOTAL: {bundle.Entry.Count(e => e.Search.Mode == Bundle.SearchEntryMode.Match)} matches ({bundle.Entry.Count} total)");
+
+            // We're re-using the first Bundle returned by the server, so the
+            // header could be slightly incorrect - but since it's internal
+            // to this class, there's no point patching it up here.
+            return bundle;
+        }
+
+        static void FetchAllRoles()
+        {
+            var client = CreateClient("https://waaseuatfhiraggregator.azurewebsites.net/fhiraggregator/fhir/");
+
+            var searchParams = new SearchParams()
+                .Add("active", "true")
+                .Include("PractitionerRole:service")
+                .Include("PractitionerRole:organization")
+                .Include("PractitionerRole:practitioner")
+                .Include("PractitionerRole:location")
+                .Include("PractitionerRole:endpoint");
+
+            // provider: name, specialty
+            // service: name, type, suburb, state, postcode
+            // endpoint: connectionType
+            var result = PagedSearch<PractitionerRole>(
+                client, 
+                searchParams);
+
+            using (var writer = new StreamWriter(@"c:\ccare\epd-r4-uat.csv"))
+            {
+                writer.WriteLine("familyName,givenNames,specialty,serviceName,serviceType,suburb,state,postcode,connectionType");
+
+                var matches = result.Entry.Where(e => e.Search.Mode == Bundle.SearchEntryMode.Match).ToArray();
+                foreach (var entry in matches)
+                {
+                    var role = (PractitionerRole) entry.Resource;
+                    var service = FindResource<HealthcareService>(result, role.HealthcareService.FirstOrDefault());
+                    var location = FindResource<Location>(result, role.Location.FirstOrDefault());
+                    var practitioner = FindResource<Practitioner>(result, role.Practitioner);
+                    var endpoint = FindResource<Endpoint>(result, role.Endpoint.FirstOrDefault());
+
+                    var name = practitioner.Name.FirstOrDefault();
+
+                    writer.WriteLine(
+                        $"{name?.Family},{string.Join(" ", name?.Given)},{role.Specialty.SelectMany(s => s.Coding).FirstOrDefault()?.Display}," +
+                        $"{service?.Name},{service.Type.SelectMany(s => s.Coding).FirstOrDefault()?.Display}," +
+                        $"{location?.Address.City},{location?.Address.State},{location?.Address.PostalCode}," +
+                        $"{endpoint?.ConnectionType.Code}");
+                }
+            }
+
         }
 
         static void FetchPractitionerRole()
@@ -156,19 +247,19 @@ namespace FhirDeps
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            var ri = new ResourceIdentity("https://sandbox.digitalhealth.gov.au/FhirServerR4-PDA/fhir/PractitionerRole/97784e9f6c7a489f9d23a40a43161e14");
+            FetchAllRoles();
 
-            FetchPractitionerRole();
-            FetchHealthcareService();
+            //FetchPractitionerRole();
+            //FetchHealthcareService();
 
             //SearchPractitionerRole();
             //TestExpand();
         }
 
         private static T FindResource<T>(Bundle bundle, ResourceReference reference) where T : Resource =>
-            reference == null 
-                ? null 
-                : bundle.Entry
+            reference == null
+                ? null :
+                bundle.Entry
                     .Where(entry => $"{entry.Resource.TypeName}/{entry.Resource.Id}" == reference.Reference) // FIXME
                     .Select(x => x.Resource)
                     .OfType<T>()
